d7969554a34c4731d6231141878376f84025e5b0
hebatist <hebaja@yahoo.com.br>
2026-03-10 21:02:59 -0300

    dealing with parser

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -7,7 +7,7 @@
 #include <limits>
 #include <sstream>
 
-bool	check_is_nan(std::string literal)
+bool	check_is_int(std::string literal)
 {
 	if (literal.find_first_not_of("0123456789") != std::string::npos)
 		return (true);
@@ -30,32 +30,38 @@ bool	is_digit(char c)
 
 bool	is_int_lmt(double num)
 {
-	if (num >= -2147483648 && num <= 2147483647)
+	if (num >= std::numeric_limits<int>::min() && num <= std::numeric_limits<int>::max())
 		return (true);
 	return (false);
 }
 
-bool	is_double_lmt(double num)
+// bool	is_double_lmt(double num)
+// {
+// 	const double	min_val = std::numeric_limits<double>::min();
+// 	const double	max_val = std::numeric_limits<double>::max();
+
+// 	if (num >= min_val && num <= max_val)
+// 		return (true);
+// 	return (false);
+// }
+
+void	print_pseudo(std::string pseudo)
 {
-	const double	min_val = std::numeric_limits<double>::min();
-	const double	max_val = std::numeric_limits<double>::max();
+	std::string	tmp;
 
-
-	if (num >= min_val && num <= max_val)
-		return (true);
-	return (false);
-}
-
-void	deal_nan(int is_minus_inf)
-{
 	std::cout << "char: impossible" << std::endl;
 	std::cout << "int: impossible" << std::endl;
-	std::cout << "float: ";
-	is_minus_inf ? std::cout << "-inff" : std::cout << "nanf";
-	std::cout << std::endl;
-	std::cout << "double: ";
-	is_minus_inf ? std::cout << "-inf" : std::cout << "nan";
-	std::cout << std::endl;
+	if (pseudo.length() == 4)
+	{
+		tmp = pseudo;
+		tmp += 'f';
+		std::cout << "float: " << tmp << std::endl;
+	}
+	else
+		std::cout << "float: " << pseudo << std::endl;
+	if (pseudo.length() == 5)
+		pseudo.erase(4);
+	std::cout << "double: " << pseudo << std::endl;
 }
 
 void ScalarConverter::convert(std::string literal)
@@ -64,14 +70,14 @@ void ScalarConverter::convert(std::string literal)
 	float	float_val;
 	double	double_val;
 
-	if (literal.compare("nan") == 0 || literal.compare("nanf") == 0)
-	{
-		deal_nan(0);
+	if (literal.length() > 1 && check_is_int(literal))
 		return ;
-	}
-	else if (literal.compare("-inf") == 0 || literal.compare("-inff") == 0)
+
+	if (literal.compare("nan") == 0 || literal.compare("nanf") == 0 
+		|| literal.compare("-inf") == 0 || literal.compare("-inff") == 0
+		|| literal.compare("+inf") == 0 || literal.compare("+inff") == 0)
 	{
-		deal_nan(1);
+		print_pseudo(literal);
 		return ;
 	}
 	else if (literal.length() == 1 && is_print(literal[0]))
@@ -95,10 +101,6 @@ void ScalarConverter::convert(std::string literal)
 		std::stringstream sd(literal);
 		sd >> double_val;
 	}
-	// float_val = static_cast<float>(int_val);
-	// double_val = static_cast<double>(int_val);
-
-
 
 	std::cout << "char: ";
 	if (is_print(int_val))
@@ -106,6 +108,7 @@ void ScalarConverter::convert(std::string literal)
 	else
 		std::cout << "Non displayable";
 	std::cout << std::endl;
+
 	std::cout << "int: ";
 	if (is_int_lmt(double_val))
 		std::cout << int_val; 
@@ -116,16 +119,20 @@ void ScalarConverter::convert(std::string literal)
 
 
 	std::cout << "float: "; 
-	std::cout << float_val << 'f';
+	double freal = std::strtod(literal.c_str(), NULL);
+	if (std::isinf(freal))
+	    std::cout << "impossible";
+	else
+		std::cout << float_val << 'f';
 	std::cout << std::endl;
 
 	std::cout << "double: ";
-	std::cout << double_val;
-	std::cout << "impossible";
-	std::cout << std::endl;
-	
 	double real = std::strtod(literal.c_str(), NULL);
 	if (std::isinf(real))
-	    std::cout << "Overflow\n";
+	    std::cout << "impossible";
+	else
+		std::cout << double_val;
+	std::cout << std::endl;
+	
 	std::cout << std::endl;
 }
